a100f3b35839bf14fc43cf107544e78277fe45af
David Ranadheer <davidbasiripogu@hotmail.com>
2024-06-18 10:47:15 +0530

    edited VideoParser_Project/settings.py

--- VideoParser_Project/settings.py
+++ VideoParser_Project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "VideoParser_Project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "Video_db",
+        "USERNAME": "root",
+        'PASSWORD': 'david96409@$A',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
